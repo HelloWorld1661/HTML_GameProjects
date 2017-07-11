e1f956280787289197cf30a859b97a2b5115d01e
JonWong1661 <Jon@Jons-MacBook-Air.local>
2017-07-11 00:21:29 -0400

    background

---
+++ Fish/js/background.js
@@ -0,0 +1,3 @@
+function drawBackround(){
+	ctx2.drawImage(bgpig,0,0,canWidth,canHeight);
+} 
--- Fish/js/main.js
+++ Fish/js/main.js
@@ -1,4 +1,36 @@
+
+var can1;
+var can2;
+
+var canWidth;
+var canHeight;
+
+var ctx1;
+var ctx2;
+
+var vbgPic = new Image();
+
 document.body.onload = game;
 function game(){
-	console.log("hello world Jun");
+	init();
+	gameloop();
+}
+
+function init(){
+	 can1 =document.getElementById("canvas1");	//fish, dust, UI circle
+	 ctx1=can1.getContext("2d");
+	 can2 =document.getElementById("canvas2");	//background
+	 ctx2=can2.getContext("2d");
+
+	 bgPic.src = "src/background.jpg";
+	 
+	 canWidth = can1.width;
+	 canHeight = can1.height;
+
+
+}
+
+function gameloop(){
+	requestAnimFrame(gameloop);
+	console.log("loop");
 }
