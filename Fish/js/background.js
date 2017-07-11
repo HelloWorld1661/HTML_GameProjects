9706e74765eb4d7c047519bb192c26c8c601b370
JonWong1661 <Jon@Jons-MacBook-Air.local>
2017-07-11 00:29:39 -0400

    Finsh the game background

--- Fish/js/background.js
+++ Fish/js/background.js
@@ -1,3 +1,3 @@
 function drawBackround(){
-	ctx2.drawImage(bgpig,0,0,canWidth,canHeight);
+	ctx2.drawImage(bgPic,0,0,canWidth,canHeight);
 } 
--- Fish/js/main.js
+++ Fish/js/main.js
@@ -8,7 +8,7 @@ var canHeight;
 var ctx1;
 var ctx2;
 
-var vbgPic = new Image();
+var bgPic = new Image();
 
 document.body.onload = game;
 function game(){
@@ -18,19 +18,20 @@ function game(){
 
 function init(){
 	 can1 =document.getElementById("canvas1");	//fish, dust, UI circle
-	 ctx1=can1.getContext("2d");
+	 ctx1=can1.getContext('2d');
 	 can2 =document.getElementById("canvas2");	//background
-	 ctx2=can2.getContext("2d");
+	 ctx2=can2.getContext('2d');
 
 	 bgPic.src = "src/background.jpg";
 	 
 	 canWidth = can1.width;
 	 canHeight = can1.height;
 
+	 drawBackround()
 
 }
 
 function gameloop(){
-	requestAnimFrame(gameloop);
+	// requestAnimFrame(gameloop);
 	console.log("loop");
 }
