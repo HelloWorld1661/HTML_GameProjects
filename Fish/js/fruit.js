5a4d59d282d7459b408f7dde32ffa410012540a0
JonWong1661 <Jon@Jons-MacBook-Air.local>
2017-07-11 07:11:57 -0400

    Fruit
    fruit grow up and flow up.

--- Fish/js/fruit.js
+++ Fish/js/fruit.js
@@ -3,6 +3,7 @@ var fruitObj = function(){
 	this.x = [];
 	this.y = [];
 	this.l = []; // fruit image's length
+	this.spd = [];
 	this.orange = new Image();
 	this.blue = new Image();
 }
@@ -14,6 +15,7 @@ fruitObj.prototype.init = function(){
 		this.alive[i] = true;
 		this.x[i] = 0;
 		this.y[i] = 0;
+		this.spd[i] = Math.random() * 0.01 + 0.005;// [0.005, 0.015)]
 		this.born(i);
 	}
 
@@ -25,7 +27,14 @@ fruitObj.prototype.draw = function(){
 	for(var i=0; i<this.num; i++){
 		// 1) draw
 		// 2) find an anemone, grow, fly up
-		this.l[i] +=0.01 * deltaTime;
+
+		if(this.l[i] <= 15){
+			this.l[i] += this.spd[i] * deltaTime;
+		}else{
+			this.y[i] -= this.spd[i] * 3 * deltaTime;
+		}
+
+		
 		// ctx2.drawImage(this.orange, this.x[i]-this.orange.width*0.5, this.y[i]-this.orange.height*0.5);
 		ctx2.drawImage(this.orange,  this.x[i]- this.l[i]*0.5,  this.y[i]- this.l[i]*0.5,  this.l[i], this.l[i] );
 	}
